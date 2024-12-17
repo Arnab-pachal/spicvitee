c73b0ba0fadfba52f4755ff80dd8ee6a72357ad1
Arnab Pachal <ap.23cs8031@nitdgp.ac.in>
2024-12-17 22:19:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Specify the output directory as 'build'
+    outDir: 'build',
   },
-})
+  base: '/my-app/', // Update this to match your app's deployment path
+});
